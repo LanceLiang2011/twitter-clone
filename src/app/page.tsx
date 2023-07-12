c5d6397e21a85dd60f70620852324b6dfe2601d8
Lance Liang <liangxianyi2011@gmail.com>
2023-07-11 23:20:47 -0400

    Build UI for sidebar and main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,83 @@
-import Link from "next/link";
-import { BiSolidHomeHeart, BiUser } from "react-icons/bi";
-import {
-  BsTwitter,
-  BsSearchHeart,
-  BsBell,
-  BsEnvelopeHeart,
-  BsBookmarkHeart,
-  BsThreeDots,
-} from "react-icons/bs";
+import SideBar from "@/components/SideBar";
 
-const NAV_ITEMS = [
-  { title: "home", icon: BiSolidHomeHeart },
-  { title: "explore", icon: BsSearchHeart },
-  { title: "notifications", icon: BsBell },
-  { title: "messages", icon: BsEnvelopeHeart },
-  { title: "bookmarks", icon: BsBookmarkHeart },
-  { title: "profile", icon: BiUser },
-];
+import { BsThreeDots } from "react-icons/bs";
 
 export default function Home() {
   return (
     <div className=" w-full h-full flex justify-center items-center relative bg-gray-950 text-white">
-      <div className=" max-w-screen-xl w-full h-full flex relative ">
+      <div className=" max-w-screen-xl w-full h-full flex relative">
         {/* Side Bar */}
-        <section className=" fixed w-72 flex flex-col items-stretch h-screen">
-          <div className=" h-full flex flex-col items-stretch mt-4 gap-2">
-            <Link href={`/`} className=" my-4 px-6">
-              <BsTwitter size={24} />
-            </Link>
-            {NAV_ITEMS.map((item) => (
-              <Link
-                className=" text-2xl self-start rounded-full px-6 py-4 flex items-center justify-center gap-4 transition duration-200 hover:bg-white/20"
-                href={`/${item.title.toLowerCase()}`}
-                key={item.title}
-              >
-                <div>
-                  <item.icon size={22} />
-                </div>
-                <div className=" capitalize">{item.title}</div>
-              </Link>
-            ))}
-            <button className=" mt-2 w-full rounded-full bg-twitterblue text-white px-6 py-4 text-2xl font-bold text-center transition duration-200 hover:bg-opacity-90">
-              Tweet
-            </button>
-          </div>
-          {/* Bottom */}
-          <button className=" flex items-center gap-2 mt-2 w-full rounded-full bg-twitterblue text-white px-6 py-4 text-2xl font-bold text-center transition duration-200 hover:bg-opacity-90">
-            {/* author icon */}
-            <div className=" rounded-full bg-slate-400 w-8 h-8"></div>
-            {/* User info */}
-            <div className="">
-              {/* User name */}
-              <div></div>
-              <div></div>
-            </div>
-            {/* 3 dots btn */}
-            <div>
-              <BsThreeDots />
-            </div>
-          </button>
-        </section>
+        <SideBar />
         {/* Home timeline */}
-        <main></main>
+        <main className=" ml-72 flex w-full h-full min-h-screen flex-col border-x-[0.5px] border-gray-600">
+          <h1 className="text-xl font-bold p-6 backdrop-blur bg-white/10 sticky top-0">
+            Home
+          </h1>
+          <div className="border-y-[0.5px] px-4 flex items-stretch py-6 space-x-2 border-gray-600 relative">
+            <div className="w-11 h-11 bg-slate-400 rounded-full flex-none"></div>
+            {/* input tweet */}
+            <form className="flex flex-col w-full h-full">
+              <input
+                type="text"
+                name="tweet"
+                className="w-full h-full text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none"
+                placeholder="What's happening?"
+              />
+              <div className="w-full justify-between items-center flex">
+                <div></div>
+                <div className="w-full max-w-[100px]">
+                  <button className="tweet-btn w-full">Tweet</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          {/* Tweets */}
+          <div className=" flex flex-col">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="border-b-[0.5px] border-gray-600 p-4 flex gap-4"
+              >
+                {/* avatar */}
+                <div>
+                  <div className="w-10 h-10 bg-slate-200 rounded-full"></div>
+                </div>
+                {/* content */}
+                <div className=" flex flex-col gap-2">
+                  {/* tweet metadata */}
+                  <div className=" flex items-center gap-1">
+                    <div className=" font-bold">Lance Liang</div>
+                    <div>@lancexliang</div>
+                    <div>
+                      <BsThreeDots />
+                    </div>
+                    <div>1 hour ago</div>
+                  </div>
+                  {/* tweet text contents */}
+                  <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Impedit minus amet possimus natus nihil in, perferendis
+                    dolor reiciendis! Eos saepe excepturi fuga quas aliquam?
+                    Molestias, provident qui ratione neque cum modi suscipit
+                    facilis, minima quo iusto amet illum labore aspernatur porro
+                    voluptas ipsam. In earum aperiam nobis ipsa doloribus
+                    sapiente.
+                  </div>
+                  {/* tweet media */}
+                  <div className=" bg-slate-400 aspect-square w-full h-96 rounded-xl"></div>
+                  {/* retweet groups */}
+                  <div className=" flex items-center gap-2 w-full">
+                    <div>C</div>
+                    <div>R</div>
+                    <div>L</div>
+                    <div>S</div>
+                    <div>Sh</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
         {/* Right panel */}
         <section></section>
       </div>
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { BiSolidHomeHeart, BiUser } from "react-icons/bi";
+import {
+  BsTwitter,
+  BsSearchHeart,
+  BsBell,
+  BsEnvelopeHeart,
+  BsBookmarkHeart,
+  BsThreeDots,
+} from "react-icons/bs";
+
+const NAV_ITEMS = [
+  { title: "", icon: BsTwitter },
+  { title: "home", icon: BiSolidHomeHeart },
+  { title: "explore", icon: BsSearchHeart },
+  { title: "notifications", icon: BsBell },
+  { title: "messages", icon: BsEnvelopeHeart },
+  { title: "bookmarks", icon: BsBookmarkHeart },
+  { title: "profile", icon: BiUser },
+];
+
+function SideBar() {
+  return (
+    <section className=" fixed w-72 flex flex-col items-stretch h-screen pr-6">
+      <div className=" h-full flex flex-col items-stretch mt-4 gap-2">
+        {/* <Link href={`/`} className=" my-4 px-6">
+      <BsTwitter size={24} />
+    </Link> */}
+        {NAV_ITEMS.map((item) => (
+          <Link
+            className=" text-2xl self-start rounded-full p-4 flex items-center justify-center gap-4 transition duration-200 hover:bg-white/20"
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+          >
+            <div>
+              <item.icon size={22} />
+            </div>
+            <div className=" capitalize">
+              {item.title !== "/" && item.title}
+            </div>
+          </Link>
+        ))}
+        <button className=" mt-2 w-full tweet-btn">Tweet</button>
+      </div>
+      {/* Bottom */}
+      <button className=" flex items-center gap-2 mt-2 w-full rounded-full bg-transparent text-white px-6 py-4 text-sm font-bold text-center transition duration-200 hover:bg-white/20">
+        {/* author icon */}
+        <div className=" rounded-full bg-slate-400 w-8 h-8"></div>
+        {/* User info */}
+        <div className="text-left text-xs">
+          {/* User name */}
+          <div className=" font-semibold text-sm">Lance Liang</div>
+          {/* User @id */}
+          <div className="">@lancexliang</div>
+        </div>
+        {/* 3 dots btn */}
+        <div className=" ml-auto text-2xl">
+          <BsThreeDots />
+        </div>
+      </button>
+    </section>
+  );
+}
+
+export default SideBar;
